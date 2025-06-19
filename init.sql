7ed1b7cef7f0020fc411763eb63e1acaa804b4ea
Allan Correa <134990755+AllanC2002@users.noreply.github.com>
2025-06-19 14:28:40 -0500

    chon: remove files
    and add secret

--- init.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE Users (
-    Id_User INT AUTO_INCREMENT PRIMARY KEY,
-    Name NVARCHAR(100),
-    Lastname NVARCHAR(100),
-    User_mail NVARCHAR(100) UNIQUE,
-    Password NVARCHAR(100),
-    Status INT CHECK (Status IN (0, 1))
-);
